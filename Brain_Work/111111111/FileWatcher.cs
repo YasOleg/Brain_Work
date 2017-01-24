ca382fc528c6df9ec0e0f631c356fd65722b7ebe
YasOleg <yasoleg94@gmail.com>
2017-01-24 16:34:09 +0200

    Add files via upload

---
+++ Brain_Work/111111111/FileWatcher.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace _111111111
+{
+
+    class FileWatcher
+    {
+        public delegate void FWatchDelegate();
+        public event FWatchDelegate FChange = null;
+
+        public static string path = "TxtWork.txt";
+
+        public void Add_Txt()
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            else
+            {                
+                File.SetLastWriteTime(path, new DateTime(1985, 4, 3));
+            }
+        }
+
+        public void EventDetectFchanges(object state)
+        {
+            if (FChange != null)
+            {
+                FChange.Invoke();
+            }
+         }
+
+        public void Start(string path)
+        {
+            DateTime dtCreated = new DateTime(1985, 4, 3);
+            DateTime dtDetectNow;
+            while (true)
+            {
+                dtDetectNow = File.GetLastWriteTime(path);
+                if (dtDetectNow != dtCreated)
+                {
+                    ThreadPool.QueueUserWorkItem(EventDetectFchanges);
+                    dtCreated = dtDetectNow;
+                }
+            }
+        }
+    }
+
+}
---
+++ Brain_Work/111111111/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace _111111111
+{
+    class Program
+    {
+        private static void Watcher_FileChanged()
+        {
+            Console.WriteLine("Enter Watcher_FileChanged");
+            string content = File.ReadAllText("TxtWork.txt");
+            if (content == "1")
+            {
+                File.WriteAllText("TxtWork.txt", "0");
+                Thread.Sleep(10000);
+            }
+            else
+            {
+                Console.WriteLine("Content: {0}. ChangeTime: {1}", content, DateTime.Now);
+            }
+        }
+        static void Main(string[] args)
+        {
+            FileWatcher FWatch = new FileWatcher();
+            FWatch.Add_Txt();
+            FWatch.FChange += Watcher_FileChanged;
+            FWatch.Start("TxtWork.txt");
+        }
+            
+    }
+}
+
+        
